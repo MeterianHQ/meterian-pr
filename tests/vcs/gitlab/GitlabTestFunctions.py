559fa27ce823bb3c9b030bea44223dd84009d6f3
Nana Insaidoo <insaidoo.nana@yahoo.it>
2024-05-20 14:11:14 +0100

    Fixed tests

--- tests/vcs/gitlab/GitlabTestFunctions.py
+++ tests/vcs/gitlab/GitlabTestFunctions.py
@@ -1,13 +1,37 @@
-from unittest.mock import Mock
+from unittest.mock import MagicMock, Mock
 from gitlab.v4.objects.projects import Project
+from gitlab.v4.objects.issues import IssueManager
+from gitlab.v4.objects.issues import Issue
+from gitlab.v4.objects.projects import ProjectManager
+from gitlab import Gitlab as PyGitlab
 
 class GitlabTestFunctions:
-    def create_project(OrgAndRepo): 
+    def create_issueless_project(OrgAndRepo): 
         tokens = OrgAndRepo.rsplit('/', 1)
         project = Mock(spec=Project)
+        project.id = 1111
         project.namespace = { 'path': tokens[0] }
         project.path = tokens[1]
         project.default_branch = "master"
         project.owner = {}
-        project.issues_enabled = True
+        return project
+
+    def create_project(OrgAndRepo): 
+        tokens = OrgAndRepo.rsplit('/', 1)
+        project = Mock(spec=Project)
+        project.id = 1111
+        project.namespace = { 'path': tokens[0] }
+        project.path = tokens[1]
+        project.default_branch = "master"
+        project.owner = {}
+        # project.issues_enabled = True DEPRECATED
+
+        pyGitlab = Mock(spec=PyGitlab)
+        projects = MagicMock(spec=ProjectManager)
+        issues = Mock(spec=IssueManager)
+
+        pyGitlab.projects = projects
+        issues.gitlab = pyGitlab
+        project.issues = issues
+
         return project
